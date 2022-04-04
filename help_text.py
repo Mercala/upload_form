197abcfeed3415c37a405458bb8afdd7c8a85e01
Mercala <63658397+Mercala@users.noreply.github.com>
2022-04-04 09:27:29 -0400

    Add files via upload

---
+++ help_text.py
@@ -0,0 +1,58 @@
+
+help = {
+    'register_value': """The **Register value** (*Leggerwaarde*) is an indication of what a house is worth, determined by an appraiser on behalf of the tax authorities.
+        The annual tax to be paid is calculated on the basis of this value.
+    """,
+    'land_tax': """"**Land tax** (*Grondbelasting*) is tax levied on land ownership.
+    """,
+    'liens': '''Liens enable Creditors to assert Rights over Property.
+        \n***Consensual liens*** are those to which you voluntarily consent, as a result of a loan or other advance of credit.
+        \n***Statutory*** liens are those to which creditors obtain security interests by the operation of state (or federal) laws.
+        \nA ***Judicial lien*** is created when a court grants a creditor an interest in the debtor's property, after a court judgment.
+    ''',
+    'gac': """**Geographic Area Classtification** (*GAC*) is a unique code that the Central Bureau of Statistics has given to an address.
+        It consists of (a) the *district code*, (b) the *neighborhood code*, and (c) the *street code*.
+    """,
+    'cadastral': """The **Cadastral number** is a unique code that the **Land Registry** has given to an immovable property.
+        It consists of (a) the *cadastral municipality code*, (b) the *cadastral section* and (c) the *parcel number*.
+    """,
+    'destination': """Government regulation on how land is used, with good spatial planning as a precondition.
+    """,
+    'designation': """**Designations** are used to further or more specifically regulate certain matters within a destination.
+        This concerns specifications for the use or construction options.
+    """,
+    'minimum_acceptable_offer': """The **minimum offer** the seller is willing to accept.
+    """,
+    'covenant': """A **covenant** is an agreement you make with an **Home Ownerowners Association** (*HOA*) that limits the way you can use a property.
+        Covenants are general rules that members of your HOA vote on that all homeowners living in the area must follow.
+    """,
+    'adu': """An **accessory dwelling unit** (*ADU*) is a legal and regulatory term for a secondary house or apartment that shares the building lot of a larger, primary home.
+        The unit cannot be bought or sold separately, but they are often used to provide additional income through rent or to house a family member.
+    """,
+    'previous_transaction': """**Previous transation** is the price this property was previously sold for.
+    """,
+    'lot_access_granted': """A **private easement** allows a neighbor to access your property.
+        A **public easement** allows any member of the public to walk through your yard.
+    """,
+    'lot_access_received': """A **private easement** allows you to access your neighbor's yard to get to your property.
+    """,
+    'percentage_commission': """The percentage of commission of the Seller's take to be paid to the referrer.
+    """,
+    'construction_size': """The total floor area of the main building on the property lot.
+    """,
+    'shareholders': """Enter the **First name**, **Last name**, and **Percentage of shares** held by each shareholder separated by a comma (See example).
+    """,
+    'refurbished': """If the roof has never been refurbished, enter the same year as the construction of the building.
+    """,
+    'vents': """**Intake vents**, located at the lowest part of the roof under the eaves, allow cool air to enter the attic.\n
+        Hot air **exhaust vents**, located at the peak of the roof, allow hot air to escape.
+    """,
+    'title': """The best way to determine if any lienholder has a financial interest in this property is to check the property's ***Certificate of Title***.
+        Ask the current owner to see the original title, not a copy, and check if any lienholder is listed.
+    """,
+    'foreclosure': """The **foreclosure value** is the amount that your home is expected to earn when it is sold on auction.
+    """,
+    'roof_framing': """A rafter is a **traditional roof framing** method in the form of elongated lumber that’s normally inclined from a central ridge beam and joins with the outer walls.
+    \nTrusses are **prefabricated structures** that are mostly made from wood. They are installed as triangular-webbing structures to support the roof.
+    """
+}
